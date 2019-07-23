a3a599246f6d395c93f6d489eba7b60429cd4116
matthew-howe <mattehowe@gmail.com>
2019-07-23 16:50:42 -0400

    Updates

---
+++ precache-manifest.7ae6c0d11bf8bb57e3ea6b8f96b7f54c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efee08a2a9192efaad12e2a379aaeba",
+    "url": "/knights-tour/index.html"
+  },
+  {
+    "revision": "6252a8e68a38ff6c4a96",
+    "url": "/knights-tour/static/css/main.063db592.chunk.css"
+  },
+  {
+    "revision": "3939675cdc840642a4d0",
+    "url": "/knights-tour/static/js/2.16b83857.chunk.js"
+  },
+  {
+    "revision": "6252a8e68a38ff6c4a96",
+    "url": "/knights-tour/static/js/main.3421c678.chunk.js"
+  },
+  {
+    "revision": "4c351a30f3aff7568b3a",
+    "url": "/knights-tour/static/js/runtime~main.26087744.js"
+  }
+]);
