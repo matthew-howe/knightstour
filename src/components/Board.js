c604aea56c22f223325a8edc63f39c6cf60a65f6
matthew-howe <mattehowe@gmail.com>
2019-08-07 12:06:18 -0400

    Adds comments to functions

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,11 +11,16 @@ class Board extends Component {
         };
     }
 
+    
     handleChange(e) {
         this.setState({ speed: e.target.value });
     }
 
     findMoves(pos) {
+        // findmoves takes the [x, y] coordinates
+        // of the knight and returns an array of valid
+        // positions for the knight to move
+
         let movesArr = [
             [pos[0] - 1, pos[1] - 2],
             [pos[0] - 2, pos[1] - 1],
@@ -33,14 +38,18 @@ class Board extends Component {
     }
 
     validMove(board, pos) {
+        // validmove checks if the target square 
+        // hasn't been visited yet
         return board[pos[0]][pos[1]] !== 1;
     }
 
     boardVisited(moves) {
+        // checks if every square has been visited
         return moves.length === 63;
     }
 
     shuffle(array) {
+        // shuffling utility
         var currentIndex = array.length,
             temporaryValue,
             randomIndex;
