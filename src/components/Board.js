3c2405305fcb05610860321155d40697c5b83499
matthew-howe <mattehowe@gmail.com>
2019-09-08 15:35:36 -0400

    Styling Updates

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,148 +1,155 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { moveKnight, updateBoard } from '../store/board';
-import backtrack from '../algorithms/backtracking'
-import warnsdorff from '../algorithms/warnsdorff'
+import backtrack from '../algorithms/backtracking';
+import warnsdorff from '../algorithms/warnsdorff';
 
 class Board extends Component {
-    constructor() {
-        super();
-        this.state = {
-            iter: 0,
-            speed: 100,
-        };
+  constructor() {
+    super();
+    this.state = {
+      iter: 0,
+      speed: 100,
+    };
 
-        this.backtrack = backtrack.bind(this)
-        this.warnsdorff = warnsdorff.bind(this)
+    this.backtrack = backtrack.bind(this);
+    this.warnsdorff = warnsdorff.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ speed: e.target.value });
+  }
+
+  renderSquare(i) {
+    let x = i % 8;
+    let y = Math.floor(i / 8);
+    let black = (x + y) % 2 === 1;
+    let knight;
+    let visited;
+    if (this.props.knight) {
+      knight = this.props.knight[0] === x && this.props.knight[1] === y;
+    }
+    if (this.props.board) {
+      visited = this.props.board[y][x] === 1;
     }
 
-    handleChange(e) {
-        this.setState({ speed: e.target.value });
+    return knight ? (
+      black ? (
+        <div key={i} className="black square knight" />
+      ) : (
+        <div key={i} className="white square knight" />
+      )
+    ) : black ? (
+      visited ? (
+        <div key={i} className="black square visited" />
+      ) : (
+        <div key={i} className="black square" />
+      )
+    ) : visited ? (
+      <div key={i} className="white square visited" />
+    ) : (
+      <div key={i} className="white square" />
+    );
+  }
+
+  render() {
+    let squares = [];
+
+    for (let i = 0; i < 64; i++) {
+      squares.push(this.renderSquare(i));
     }
-
-    renderSquare(i) {
-        let x = i % 8;
-        let y = Math.floor(i / 8);
-        let black = (x + y) % 2 === 1;
-        let knight;
-        let visited;
-        if (this.props.knight) {
-            knight = this.props.knight[0] === x && this.props.knight[1] === y;
-        }
-        if (this.props.board) {
-            visited = this.props.board[y][x] === 1;
-        }
-
-        return knight ? (
-            black ? (
-                <div key={i} className="black square knight" />
-            ) : (
-                    <div key={i} className="white square knight" />
-                )
-        ) : black ? (
-            visited ? (
-                <div key={i} className="black square visited" />
-            ) : (
-                    <div key={i} className="black square" />
-                )
-        ) : visited ? (
-            <div key={i} className="white square visited" />
-        ) : (
-                        <div key={i} className="white square" />
-                    );
-    }
-
-    render() {
-        let squares = [];
-
-        for (let i = 0; i < 64; i++) {
-            squares.push(this.renderSquare(i));
-        }
-        return (
-            <div id="main">
-                <div id="board">{squares}</div>
-                <div id="buttons">
-                    <button
-                        onClick={() =>
-                            this.warnsdorff(
-                                this.props.board,
-                                [[5, 3]],
-                                this.props.updateBoard,
-                                this.props.moveKnight
-                            )
-                        }
-                        id="b3"
-                    >
-                        Divide and Conquer
-                    </button>
-                    <button
-                        onClick={() =>
-                            this.warnsdorff(
-                                this.props.board,
-                                [[5, 3]],
-                                this.props.updateBoard,
-                                this.props.moveKnight,
-                                this.state.speed
-                            )
-                        }
-                        id="b3"
-                    >
-                        Warnsdorf's Algorithm
-                    </button>
-                    <button
-                        onClick={() =>
-                            this.backtrack(
-                                this.props.board,
-                                [[0, 0]],
-                                this.props.updateBoard,
-                                this.props.moveKnight,
-                                this.state.speed
-                            )
-                        }
-                        id="b4"
-                    >
-                        Brute Force Permutations
-                    </button>
-                    <div />
-                    <p className="iterations">
-                   Iterations:{' '}
-                        {this.state.iter}
-                    </p>
-                    <div>
-                    <p className="speed-text">
-                       Speed: {this.state.speed}{' '}
-                        ms
-                    </p>
-                    <input
-                        name="speed"
-                        onChange={e => {
-                            this.handleChange(e);
-                        }}
-                        type="range"
-                        min="10"
-                        max="500"
-                        value={this.state.speed}
-                        className="slider"
-                    />
-                </div>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div id="main">
+        <div id="board">{squares}</div>
+        <div id="buttons">
+          <button
+            onClick={() =>
+              this.backtrack(
+                this.props.board,
+                [[0, 0]],
+                this.props.updateBoard,
+                this.props.moveKnight,
+                this.state.speed
+              )
+            }
+            id="b4"
+          >
+            Brute Force Iterations
+          </button>
+          <button
+            onClick={() =>
+              this.warnsdorff(
+                this.props.board,
+                [[5, 3]],
+                this.props.updateBoard,
+                this.props.moveKnight,
+                this.state.speed
+              )
+            }
+            id="b3"
+          >
+            Warnsdorf's Algorithm
+          </button>
+          <button
+            onClick={() =>
+              this.warnsdorff(
+                this.props.board,
+                [[5, 3]],
+                this.props.updateBoard,
+                this.props.moveKnight
+              )
+            }
+            id="b3"
+          >
+            Divide and Conquer
+          </button>
+          <button
+            onClick={() =>
+              this.warnsdorff(
+                this.props.board,
+                [[5, 3]],
+                this.props.updateBoard,
+                this.props.moveKnight
+              )
+            }
+            id="b3"
+          >
+              Neural Network Algorithm
+          </button>
+          <div />
+          <div>
+            <p className="speed-text">Speed: {this.state.speed} ms</p>
+            <input
+              name="speed"
+              onChange={e => {
+                this.handleChange(e);
+              }}
+              type="range"
+              min="10"
+              max="500"
+              value={this.state.speed}
+              className="slider"
+            />
+          </div>
+          <p className="iterations">Iterations: {this.state.iter}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapState = state => ({
-    board: state.board.board,
-    knight: state.board.knight,
-    iterations: state.board.iterations,
+  board: state.board.board,
+  knight: state.board.knight,
+  iterations: state.board.iterations,
 });
 
 const mapDispatch = dispatch => ({
-    moveKnight: knight => dispatch(moveKnight(knight)),
-    updateBoard: newBoard => dispatch(updateBoard(newBoard)),
+  moveKnight: knight => dispatch(moveKnight(knight)),
+  updateBoard: newBoard => dispatch(updateBoard(newBoard)),
 });
 
 export default connect(
-    mapState,
-    mapDispatch
+  mapState,
+  mapDispatch
 )(Board);
