e8905c51feb5c4d59122a653c99df3c92368f0b5
matthew-howe <mattehowe@gmail.com>
2019-09-29 00:18:58 -0400

    Adds param comments

--- src/algorithms/backtracking.js
+++ src/algorithms/backtracking.js
@@ -1,68 +1,58 @@
 import util from '../utils/utils';
 
-const backtrack = async (board, moves, updateBoard, moveKnight, speed, iterate, updateSpeed) => {
-    await setTimeout(async () => {
+// @param {array[][]} board - 2d array of the board
+// @param {array[][] moves - 2d array of the moves so far
+// @param {object{}} updateBoard - function to update board state
+// @param {object{}} moveKnight - function to update knight state
+// @param {integer} speed - ms speed for each iteration
+// @param {void} iterate - function to change local iteration state
+// @param {object{}} updateSpeed - function to update speed state
+const backtrack = async (
+  board,
+  moves,
+  updateBoard,
+  moveKnight,
+  speed,
+  iterate,
+  updateSpeed
+) => {
+  setTimeout(async () => {
+    iterate();
+    let curBoard = board;
+    curBoard[0][0] = 1;
 
-        iterate()
-        let curBoard = board; 
-        curBoard[0][0] = 1; // <- remove later
+    if (util.boardVisited(moves)) return true;
 
-        
-      if (util.boardVisited(moves)) return true; // break case
+    const curPosition = moves[moves.length - 1];
+    let possibleMoves = util.findMoves(curPosition);
+    possibleMoves = util.shuffle(possibleMoves);
 
-      // get the current position of the knight and the
-      // possible moves for it to make
-      const lastMove = moves[moves.length - 1];
-      let possibleMoves = util.findMoves(lastMove);
-      possibleMoves = util.shuffle(possibleMoves);
+    for (let moveIdx = 0; moveIdx < possibleMoves.length; moveIdx++) {
+      const curMove = possibleMoves[moveIdx];
+      if (util.validMove(curBoard, curMove)) {
+        moves.push(curMove);
+        let row = curMove[0];
+        let column = curMove[1];
 
+        curBoard[row][column] = 1;
 
-    // for each possible move check if its a valid move
-    // if it is, make that move
-      for (let moveIdx = 0; moveIdx < possibleMoves.length; moveIdx++) {
-        const curMove = possibleMoves[moveIdx];
-        if (util.validMove(curBoard, curMove)) {
-          moves.push(curMove);
-          let row = curMove[0];
-          let column = curMove[1];
-
-          curBoard[row][column] = 1;
-
-          moveKnight(curMove);
-          updateBoard(curBoard);
-          if (backtrack(curBoard, moves, updateBoard, moveKnight, speed, iterate)) {
-            return true;
-          }
-
-          moves.pop();
-          curBoard[curMove[0]][curMove[1]] = 0;
-          moveKnight(moves[moves.length - 1]);
-          updateBoard(curBoard);
+        moveKnight(curMove);
+        updateBoard(curBoard);
+        if (
+          backtrack(curBoard, moves, updateBoard, moveKnight, speed, iterate)
+        ) {
+          return true;
         }
+
+
       }
+    }
 
-      curBoard[lastMove[0]][lastMove[1]] = 0;
-      moves.pop();
+    console.log('ended', 
+      moves[moves.length - 1]
+    )
 
-      moveKnight(moves[moves.length - 2]);
-      updateBoard(curBoard);
-        backtrack(
-            [
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-                [0,0,0,0,0,0,0,0,0,0,0,0], 
-            ], 
-            [[0,0]], updateBoard, moveKnight, speed, iterate);
-    }, speed);
-  }
+  }, 10);
+};
 
 export default backtrack;
