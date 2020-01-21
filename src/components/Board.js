0eab958861b8b17dc28d22455567db530031ae98
matthew-howe <mattehowe@gmail.com>
2020-01-21 15:38:00 -0500

    Revert "fixes jest/react-app conflict"
    This reverts commit ef8b6104679ef871481b36474a951a591078d135.

--- src/components/Board.js
+++ src/components/Board.js
@@ -111,6 +111,11 @@ class Board extends Component {
     return (
       <div id="main">
         <div id="img">
+          <img
+            className="img"
+            alt="knight"
+            src="https://i.imgur.com/rDN4qFr.jpg"
+          />
         </div>
         <div className="middle">
           <div id="title">
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,11 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {createLogger} from 'redux-logger';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import board from './board';
+import thunk from 'redux-thunk';
 
 const reducer = combineReducers({board});
 const middleware = composeWithDevTools(
-  applyMiddleware(createLogger({collapsed: true})),
+  applyMiddleware(createLogger({collapsed: true}, thunk)),
 );
 const store = createStore(reducer, middleware);
 
