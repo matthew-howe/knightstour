9f3179335908f4afd589412bf1fe8e8b9f007d95
matthew-howe <mattehowe@gmail.com>
2020-02-27 13:32:10 -0500

    typescript conversion

---
+++ src/algorithms/warnsdorf-anime.tsx
@@ -0,0 +1,55 @@
+import actionQueue from "../queue/action-queue";
+
+const warnsdorfAnime = (updateBoard: any, moveKnight: any) => {
+
+  actionQueue.clear();
+
+    const board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ]
+      const knight = [0, 0]
+
+      actionQueue.startQueueing(80)
+
+      for (let i = 1; i < 1000; i++) {
+
+
+        
+
+        knight[0] = Math.floor(Math.random() * 12);
+        knight[1] = Math.floor(Math.random() * 12);
+        
+        
+        board[Math.floor(Math.random() * 12)][Math.floor(Math.random() * 12)] = 1
+        
+        let newBoard:number[][] = [[]];
+
+        for (let i = 0; i < 12; i++) {
+            newBoard[i] = board[i].slice();
+        }
+
+        let newKnight = knight.slice()
+        
+        actionQueue.enqueue(() => {         
+
+            updateBoard(newBoard)
+            moveKnight(newKnight)
+        })
+        
+      }
+    
+      
+}
+
+export default warnsdorfAnime;
