bb0144de283f143d2f62edcc8883a0fe5e9876df
matthew-howe <mattehowe@gmail.com>
2022-07-17 20:13:07 -0400

    Updates

--- precache-manifest.3340ca9a7373dc3f00ae005d3a8685d5.js
+++ precache-manifest.3340ca9a7373dc3f00ae005d3a8685d5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "466fb9167ef2cd025620c63d53f6a299",
+    "revision": "838bdd60e54f918d94f13e8b461fc2bf",
     "url": "/knightstour/index.html"
   },
   {
-    "revision": "58ed8fcac1171329cd15",
+    "revision": "c2f0f8cbb1131f46309e",
     "url": "/knightstour/static/css/main.dd5ba4dc.chunk.css"
   },
   {
-    "revision": "e976ed72f2fdc3aa9b82",
-    "url": "/knightstour/static/js/2.5095a9a4.chunk.js"
+    "revision": "40eb83603b06ae019a97",
+    "url": "/knightstour/static/js/2.c6469f3a.chunk.js"
   },
   {
-    "revision": "58ed8fcac1171329cd15",
-    "url": "/knightstour/static/js/main.cc4974bb.chunk.js"
+    "revision": "c2f0f8cbb1131f46309e",
+    "url": "/knightstour/static/js/main.6bcc96a9.chunk.js"
   },
   {
     "revision": "9ab319ed013a369a5deb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/knightstour/precache-manifest.3135a2fd847125309580b8446be62550.js"
+  "/knightstour/precache-manifest.3340ca9a7373dc3f00ae005d3a8685d5.js"
 );
 
 self.addEventListener('message', (event) => {
